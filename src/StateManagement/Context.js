3a0cc379ab585f8f77c76778c07d907a0b82bcea
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2023-01-19 17:24:32 +0545

    setup

--- src/StateManagement/Context.js
+++ src/StateManagement/Context.js
@@ -1,18 +1,21 @@
-import React, { createContext, useContext, useReducer } from 'react';
-
+import React, { createContext, useContext, useReducer } from "react";
+import reducer from "./reducer";
 const AppContext = createContext();
 const initialState = {
-    value: ""
-  };
+  value: "",
+};
 
-const AppProvider = ({children}) => {
-    const [state, dispatch] = useReducer(reducer, initialState);
-    return <AppContext.Provider value={{ ...state, dispatch }}>{children}</AppContext.Provider>;
-}
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <AppContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
 
 const useGlobalContext = () => {
-    return useContext(AppContext);
-  };
-  
-  export { useGlobalContext, AppProvider };
-  
+  return useContext(AppContext);
+};
+
+export { useGlobalContext, AppProvider };
--- src/StateManagement/reducer.js
+++ src/StateManagement/reducer.js
@@ -1,14 +1,12 @@
-
-
-const reducer = (state, action)=>{
-    switch(action.type){
-        case "UPDATE_USER": {
-        return {...state}
-        }
-        default:{
-            return{...state}
-        }
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "UPDATE_USER": {
+      return { ...state };
     }
-}
+    default: {
+      return { ...state };
+    }
+  }
+};
 
-export default reducer
+export default reducer;
