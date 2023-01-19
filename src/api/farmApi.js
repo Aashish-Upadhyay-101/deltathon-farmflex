13316f23b93896eda37d31d04242016a45821c26
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2023-01-20 04:30:09 +0545

    dashboard, landing page update

---
+++ src/api/farmApi.js
@@ -0,0 +1,64 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const farmApi = createApi({
+  reducerPath: "farmApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://127.0.0.1:8000/api/",
+  }),
+  prepareHeaders: (headers, { getState }) => {
+    headers.set("Content-Type", "application/json");
+    headers.set("Authorization", `Bearer ${localStorage.getItem("access")}`);
+  },
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query(data) {
+        return {
+          method: "POST",
+          url: "user/login/",
+          body: data,
+        };
+      },
+    }),
+    signup: builder.mutation({
+      query(data) {
+        return {
+          method: "POST",
+          url: "user/signup/",
+          body: data,
+        };
+      },
+    }),
+    sendVerificationEmail: builder.mutation({
+      query: (username) => `user/activate-link/${username}/`,
+    }),
+
+    profileUpdate: builder.mutation({
+      query(data) {
+        return {
+          method: "PATCH",
+          url: `profile/${data.username}/`,
+          body: data,
+        };
+      },
+    }),
+
+    getStores: builder.query({
+      query: () => "store/all/",
+    }),
+
+    getStoreProducts: builder.query({
+      query: (store_id) => `store/${store_id}/get-all-products/`,
+    }),
+
+    bookStore: builder.mutation({}),
+  }),
+});
+
+export const {
+  useLoginMutation,
+  useSignupMutation,
+  useSendVerificationEmailMutation,
+  useProfileUpdateMutation,
+  useGetStoresQuery,
+  use,
+} = farmApi;
